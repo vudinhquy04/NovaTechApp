5178754efa4bffa7ab3f2f55bcb0cea7e72629b5
Your Name <vudinhquyk4@gmail.com>
2026-01-26 22:09:42 +0700

    update

--- backend/utils/tokenUtils.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '7d'
-  });
-};
-
-module.exports = { generateToken };
